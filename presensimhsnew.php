0fe7bd18cad9ca4b867466e4fcd1243ad13d960a
YodiYanwar <yodi.yanwar@gmail.com>
2018-05-16 08:22:11 +0700

    update

--- presensimhsnew.php
+++ presensimhsnew.php
@@ -43,13 +43,13 @@
 	/*$sql = "SELECT USERID, Format(DateValue(CHECKTIME), 'd/mm/yyyy') AS tgl, Format(TimeValue(CHECKTIME)) AS wkt FROM CHECKINOUT WHERE USERID = 1352 AND Format(DateValue(CHECKTIME), 'd/mm/yyyy') BETWEEN #18/09/2017# AND #20/09/2017# ORDER BY Format(DateValue(CHECKTIME)), Format(TimeValue(CHECKTIME))";
 	*/
 
-	$sql = "SELECT userid AS id_mahasiswa, Format(DateValue(c.CHECKTIME), 'dd-mm-yyyy') AS tgl, Format(TimeValue(Min(t.CHECKTIME))) As wkt_tapping, session AS wkt_shalat FROM (SELECT session_from, session_to, date_from, date_to, session FROM ((timesetup i LEFT JOIN dateperiod d ON i.period_id = d.period_id) LEFT JOIN sessionrange q ON i.sessionrange_id = q.sessionrange_id)) As s INNER JOIN (SELECT Format(DateValue(CHECKTIME)) As tanggal, Format(TimeValue(CHECKTIME)) As tapping, u.userid, u.Badgenumber, CHECKTIME FROM CHECKINOUT c LEFT JOIN USERINFO u ON c.userid = u.userid WHERE (Format(DateValue(c.CHECKTIME), 'yyyy-mm-dd')  BETWEEN '2018-04-04' AND '2018-04-05') AND (u.Badgenumber LIKE '17%')) t ON ((t.tanggal BETWEEN s.date_from AND s.date_to) AND (t.tapping BETWEEN s.session_from AND s.session_to)) GROUP BY userid, Format(DateValue(c.CHECKTIME), 'dd-mm-yyyy'), session ORDER BY userid, Format(DateValue(c.CHECKTIME), 'dd-mm-yyyy'), Format(TimeValue(Min(t.CHECKTIME)))";
+	$sql = "SELECT userid AS id_mahasiswa, Format(DateValue(c.CHECKTIME), 'dd-mm-yyyy') AS tgl, Format(TimeValue(Min(t.CHECKTIME))) As wkt_tapping, session AS wkt_shalat FROM (SELECT session_from, session_to, date_from, date_to, session FROM ((timesetup i LEFT JOIN dateperiod d ON i.period_id = d.period_id) LEFT JOIN sessionrange q ON i.sessionrange_id = q.sessionrange_id)) As s INNER JOIN (SELECT Format(DateValue(CHECKTIME)) As tanggal, Format(TimeValue(CHECKTIME)) AS tapping, u.userid, u.Badgenumber, CHECKTIME FROM CHECKINOUT c LEFT JOIN USERINFO u ON c.userid = u.userid WHERE (Format(DateValue(c.CHECKTIME), 'yyyy-mm-dd')  BETWEEN '2018-04-23' AND '2018-04-30') AND (u.Badgenumber LIKE '17%')) t ON ((t.tanggal BETWEEN s.date_from AND s.date_to) AND (t.tapping BETWEEN s.session_from AND s.session_to)) GROUP BY userid, Format(DateValue(c.CHECKTIME), 'dd-mm-yyyy'), session ORDER BY userid, Format(DateValue(c.CHECKTIME), 'dd-mm-yyyy'), Format(TimeValue(Min(t.CHECKTIME)))";
 
 	//$angkatan = "1710";
 
 	//$sql = "SELECT USERID, Badgenumber, Name FROM USERINFO WHERE Badgenumber LIKE '1710%' AND Name NOT LIKE '17%' ORDER BY Name";
 
-	$sqlTotal = "SELECT COUNT(userid) AS Total FROM USERINFO WHERE Badgenumber LIKE '1710%'";
+	$sqlTotal = "SELECT COUNT(userid) AS Total FROM USERINFO WHERE Badgenumber LIKE '17%'";
 	//$sql = "SELECT userid, Badgenumber, Name FROM USERINFO WHERE Badgenumber LIKE '17%' ORDER BY Badgenumber";
 
 	$resultTotal = odbc_exec($koneksi, $sqlTotal);
